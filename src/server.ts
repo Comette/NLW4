a9af47e33900e769d26cb8e1da04ebb866b92366
Victor Comette <victor.comette@gmail.com>
2021-02-23 20:53:49 -0300

    feat: Aula 02

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,12 @@
-import express, { request, response } from 'express';
+import "reflect-metadata";
+import "./database";
+
+import express, { Request, Response } from 'express';
+import { router } from "./routes";
 
 const app = express();
 
-app.get("/", (request, response) => {
-  return response.json({ message: "Hello Word - NLW04" });
-});
-
-app.post("/", (request, response) => {
-  return response.json({ message: "Os dados foram salvos com sucesso!" });
-})
+app.use(express.json());
+app.use(router);
 
 app.listen(3333, () => console.log("Server is running!!"));
